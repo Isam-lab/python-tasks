892f4309b78ce2afd7a7b265c644b8b6008b41c5
Isam Al-Shehabi <isam.al-shehabi@gmx.de>
2025-04-11 10:37:25 +0200

    password_checker is done

--- password_checker.py
+++ password_checker.py
@@ -1,2 +1,18 @@
 def run():
-    print("Hallo, Welt! Ich bin Isam!")
+    print("\n--- Option 9: Passwort-Checker ---")
+
+    pw = input("Bitte gib ein Passwort ein: ")
+
+    
+    lang_genug = len(pw) >= 8
+    hat_zahl = any(char.isdigit() for char in pw)
+
+    if lang_genug and hat_zahl:
+        print("✅ Starkes Passwort!")
+    else:
+        print("❌ Schwaches Passwort.")
+        if not lang_genug:
+            print("- Es muss mindestens 8 Zeichen lang sein.")
+        if not hat_zahl:
+            print("- Es muss mindestens eine Zahl enthalten.")
+
